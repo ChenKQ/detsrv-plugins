a1bb796fc1a206b997d7f360603369d44e814495
chenkq <812545175@qq.com>
2021-08-15 23:12:46 +0800

    complete trtyolov5

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include <fstream>
 #include <sstream>
 #include <memory>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char* argv[])
 {
-    const char* imgFile = "dump.jpg";
+    const char* imgFile = argv[1];
     std::ifstream fstrm(imgFile, std::ios::binary);
     if(!fstrm.is_open())
     {
@@ -19,14 +20,41 @@ int main(int argc, const char* argv[])
     std::stringstream sstrm;
     sstrm << fstrm.rdbuf();
     std::string str = sstrm.str();
+    // const unsigned char* pData = (const unsigned char*)str.c_str();
+    // std::vector<unsigned char> imgVec(pData, pData+str.length());
+
+    // cv::Mat img = cv::imdecode(imgVec, cv::ImreadModes::IMREAD_UNCHANGED );
+
+    // cv::imwrite("save.jpg", img);
+    
 
     auto pDetector = detsvr::createInstance();
-    detsvr::DetectionResult result = pDetector->detect(str.c_str(), str.length());
+    // warmup
+    for(int i = 0; i<5; ++i)
+    {
+        detsvr::DetectionResult result = pDetector->detect(str.c_str(), str.length());
 
-    std::cout   << "{img_width: " << result.img_width 
-                << ", img_height: " << result.img_height
-                << ", pre_time: " << result.pre_time
-                << ", inf_time: " << result.inf_time
-                << ", list: " << result.list.size() << "}\n";
+        std::cout   << "{img_width: " << result.img_width 
+                    << ", img_height: " << result.img_height
+                    << ", pre_time: " << result.pre_time
+                    << ", inf_time: " << result.inf_time
+                    << ", list: " << result.list.size() << "}\n";
+    }
+    const size_t testRepeat = 10;
+    auto start = std::chrono::high_resolution_clock::now();
+    for(int i = 0; i<testRepeat; ++i)
+    {
+        detsvr::DetectionResult result = pDetector->detect(str.c_str(), str.length());
+
+        // std::cout   << "{img_width: " << result.img_width 
+        //             << ", img_height: " << result.img_height
+        //             << ", pre_time: " << result.pre_time
+        //             << ", inf_time: " << result.inf_time
+        //             << ", list: " << result.list.size() << "}\n";
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    int total = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    
+    std::cout << "total time: " << total << "; mean: " << total/testRepeat << std::endl;
     return 0;
 }
--- trtyolov5/trtyolov5.h
+++ trtyolov5/trtyolov5.h
@@ -3,24 +3,58 @@
 
 #include "detsvr/IDetect.h"
 #include "detsvr/detectionresult.h"
+#include "logging.h"
+#include "yololayer.h"
+#include "NvInfer.h"
+#include "NvInferRuntime.h"
+#include "common.hpp"
+#include <opencv2/opencv.hpp>
+#include <tuple>
 
 namespace detsvr
 {
 
-class DetectionMinic final : public IDetect 
+class DetectionYoloV5 final : public IDetect 
 {
 public:
-    DetectionMinic();
-    virtual ~DetectionMinic() override = default;
+    DetectionYoloV5();
+    virtual ~DetectionYoloV5();
 
     DetectionResult detect(const char* data, size_t length) override; 
 
 private:
+    void preprocessImg(cv::Mat& img);
+    void doInference(float* input, float* output);
+
+private:
+    cudaStream_t stream;
+    void* buffers[2];
+    cv::Mat decodedImage;
+    cv::Mat resizedImage;
+    cv::Mat preprocessedImage;
+    std::unique_ptr<float[]> data {nullptr};
+    std::unique_ptr<float[]> prob {nullptr};
+
+    IRuntime* runtime = nullptr;
+    ICudaEngine* engine = nullptr;
+    IExecutionContext* context = nullptr;
+
     const int deviceID = 0;
-    // const double nmsThresh = 0.4;
-    // const double confidenceThresh = 0.5;
-    // const int batchSize = 1;
-    const std::string engineName = "trtyolov5.trt";
+    const double nmsThresh = 0.4;
+    const double confidenceThresh = 0.5;
+    
+    const std::string engineName = "trtyolov5.engine";
+    const int batchSize = 1;
+    const int inputWidth = Yolo::INPUT_W;
+    const int inputHeight = Yolo::INPUT_H;
+    const int maxOutputBBoxCount = Yolo::MAX_OUTPUT_BBOX_COUNT;
+    const int classNumber = Yolo::CLASS_NUM;
+    const int outputSize = Yolo::MAX_OUTPUT_BBOX_COUNT * sizeof(Yolo::Detection) / sizeof(float) + 1;
+
+    const char* inputBlobName = "data";
+    const char* outputBlobName = "prob";
+
+    Logger gLogger;
 };
 
 
