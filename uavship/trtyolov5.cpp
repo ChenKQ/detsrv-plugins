eded851503c60e453c8c571a3df64718e8ceaf2e
chenkq <812545175@qq.com>
2021-08-18 01:09:01 +0800

    remove redundancy

---
+++ uavship/trtyolov5.cpp
@@ -0,0 +1,206 @@
+#include "trtyolov5.h"
+#include "detsvr/IDetect.h"
+#include "cuda_utils.h"
+#include <fstream>
+#include <iostream>
+#include <cassert>
+#include <memory>
+#include <opencv2/imgcodecs.hpp>
+#include <algorithm>
+
+namespace detsvr
+{
+
+DetectionYoloV5::DetectionYoloV5()
+{
+    cudaSetDevice(deviceID);
+
+    std::ifstream file(engineName, std::ios::binary);
+    if (!file.good()) {
+        std::cerr << "read " << engineName << " error!" << std::endl;
+        throw std::runtime_error(std::string{"cannot load engine file: "} + engineName);
+    }
+
+    char* trtModelStream = nullptr;
+    size_t size = 0;
+    file.seekg(0, file.end);
+    size = file.tellg();
+    file.seekg(0, file.beg);
+    trtModelStream = new char[size];
+    assert(trtModelStream);
+    file.read(trtModelStream, size);
+    file.close();
+
+    // prepare input data ---------------------------
+    float *pData = new float[batchSize * 3 * inputHeight * inputHeight];
+    data.reset(pData);
+    float *pProb = new float[batchSize * outputSize];
+    prob.reset(pProb);
+
+    runtime = createInferRuntime(gLogger);
+    assert(runtime != nullptr);
+    engine = runtime->deserializeCudaEngine(trtModelStream, size);
+    assert(engine != nullptr);
+    context = engine->createExecutionContext();
+    assert(context != nullptr);
+    delete[] trtModelStream;
+    assert(engine->getNbBindings() == 2);
+    // In order to bind the buffers, we need to know the names of the input and output tensors.
+    // Note that indices are guaranteed to be less than IEngine::getNbBindings()
+
+    const int inputIndex = engine->getBindingIndex(inputBlobName);
+    const int outputIndex = engine->getBindingIndex(outputBlobName);
+    assert(inputIndex == 0);
+    assert(outputIndex == 1);
+
+    CUDA_CHECK(cudaMalloc(&buffers[inputIndex], batchSize * 3 * inputHeight * inputWidth * sizeof(float)));
+    CUDA_CHECK(cudaMalloc(&buffers[outputIndex], batchSize * outputSize * sizeof(float)));
+
+    CUDA_CHECK(cudaStreamCreate(&stream));
+}
+
+DetectionYoloV5::~DetectionYoloV5()
+{
+    // Release stream and buffers
+    cudaStreamDestroy(stream);
+    const int inputIndex = engine->getBindingIndex(inputBlobName);
+    const int outputIndex = engine->getBindingIndex(outputBlobName);
+    CUDA_CHECK(cudaFree(buffers[inputIndex]));
+    CUDA_CHECK(cudaFree(buffers[outputIndex]));
+    // Destroy the engine
+    context->destroy();
+    engine->destroy();
+    runtime->destroy();
+}
+
+void DetectionYoloV5::doInference(float* input, float* output) 
+{
+    // DMA input batch data to device, infer on the batch asynchronously, and DMA output back to host
+    CUDA_CHECK(cudaMemcpyAsync(buffers[0], input, batchSize * 3 * inputHeight * inputWidth * sizeof(float), cudaMemcpyHostToDevice, stream));
+    context->enqueue(batchSize, buffers, stream, nullptr);
+    CUDA_CHECK(cudaMemcpyAsync(output, buffers[1], batchSize * outputSize * sizeof(float), cudaMemcpyDeviceToHost, stream));
+    cudaStreamSynchronize(stream);
+}
+
+DetectionResult DetectionYoloV5::detect(const char* rawData, size_t length)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    const unsigned char* pData = (const unsigned char*)rawData;
+    std::vector<unsigned char> imgVec(pData, pData+ length);
+    cv::Mat img = cv::imdecode(imgVec, cv::ImreadModes::IMREAD_UNCHANGED);
+    if (img.empty()) return {};
+    preprocessImg(img); // letterbox BGR to RGB
+    auto end = std::chrono::high_resolution_clock::now();
+    int pretime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    // Run inference
+    start = std::chrono::high_resolution_clock::now();
+    doInference(data.get(), prob.get());
+    // end = std::chrono::high_resolution_clock::now();
+    std::vector<std::vector<Yolo::Detection>> batch_res(1);
+    for (int b = 0; b < batchSize; b++) {
+        auto& res = batch_res[b];
+        nms(res, &prob[b * outputSize], confidenceThresh, nmsThresh);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    int inftime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    
+    DetectionResult result;
+    result.img_tag = "";
+    result.img_time = "";
+    result.img_height = img.rows;
+    result.img_width = img.cols;
+    result.pre_time = pretime;
+    result.inf_time = inftime;
+    result.list.clear();
+    int count = 0;
+    for (int b = 0; b < batchSize; b++) {
+        auto& res = batch_res[b];
+        for (size_t j = 0; j < res.size(); j++) {
+            // std::cout << "box: " << res[j].bbox[0] << ":" << res[j].bbox[1] << ":"
+            //           << res[j].bbox[2] << ":" << res[j].bbox[3] << "\n";
+            cv::Rect r = get_rect(img, res[j].bbox);
+            std::string name = std::to_string(static_cast<int>(res[j].class_id));
+            double prob = res[j].conf;
+            int minx = std::max(r.x, 0);
+            int maxx = std::min(r.x + r.width, img.cols);
+            if(minx>=maxx)
+            {
+                continue;
+            }
+            int miny = std::max(r.y, 0);
+            int maxy = std::min(r.y + r.height, img.rows);
+            if(miny>=maxy)
+            {
+                continue;
+            }
+            ++count;
+            // std::cout << "height is " << (maxy-miny) << "\n";
+            BBox box {count, name, prob, minx, maxx, miny, maxy};
+            result.list.push_back(box);
+            cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+            cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+        }
+        cv::imwrite("saved.png", img);
+    }
+    
+    return std::move(result);
+}
+
+void DetectionYoloV5::preprocessImg(cv::Mat& img)
+{
+    int w, h, x, y;
+    float r_w = inputWidth / (img.cols*1.0);
+    float r_h = inputHeight / (img.rows*1.0);
+    if (r_h > r_w) {
+        w = inputWidth;
+        h = r_w * img.rows;
+        x = 0;
+        y = (inputHeight - h) / 2;
+    } else {
+        w = r_h * img.cols;
+        h = inputHeight;
+        x = (inputWidth - w) / 2;
+        y = 0;
+    }
+
+    if((resizedImage.rows!=h) || (resizedImage.cols!=w))
+    {
+        resizedImage = cv::Mat(h, w, CV_8UC3);
+    }
+    
+    if(preprocessedImage.empty())
+    {
+        preprocessedImage = cv::Mat(inputHeight, inputWidth, CV_8UC3, cv::Scalar(128, 128, 128));
+    }
+
+    // auto start = std::chrono::system_clock::now();
+    cv::resize(img, resizedImage, resizedImage.size(), 0, 0, cv::INTER_LINEAR);
+    resizedImage.copyTo(preprocessedImage(cv::Rect(x, y, resizedImage.cols, resizedImage.rows)));
+    // auto end = std::chrono::system_clock::now();
+    // int pretime = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    // std::cout << "preprocess time: " << pretime << "us" << std::endl;
+    int i = 0;
+    for (int row = 0; row < inputHeight; ++row) {
+        uchar* uc_pixel = preprocessedImage.data + row * preprocessedImage.step;
+        for (int col = 0; col < inputWidth; ++col) {
+            data[i] = (float)uc_pixel[2] / 255.0;
+            data[i + inputHeight * inputWidth] = (float)uc_pixel[1] / 255.0;
+            data[i + 2 * inputHeight * inputWidth] = (float)uc_pixel[0] / 255.0;
+            uc_pixel += 3;
+            ++i;
+        }
+    }
+    // return;
+}
+
+
+std::shared_ptr<IDetect> createInstance()
+{
+    IDetect* ptr = new DetectionYoloV5();
+    return std::shared_ptr<IDetect>(ptr);
+}
+
+} // namespace detsvr
+
---
+++ uavship/trtyolov5.h
@@ -0,0 +1,63 @@
+#ifndef _DETSVR_DETECTION_MINIC_
+#define _DETSVR_DETECTION_MINIC_
+
+#include "detsvr/IDetect.h"
+#include "detsvr/detectionresult.h"
+#include "logging.h"
+#include "yololayer.h"
+#include "NvInfer.h"
+#include "NvInferRuntime.h"
+#include "common.hpp"
+#include <opencv2/opencv.hpp>
+#include <tuple>
+
+namespace detsvr
+{
+
+class DetectionYoloV5 final : public IDetect 
+{
+public:
+    DetectionYoloV5();
+    virtual ~DetectionYoloV5();
+
+    DetectionResult detect(const char* data, size_t length) override; 
+
+private:
+    void preprocessImg(cv::Mat& img);
+    void doInference(float* input, float* output);
+
+private:
+    cudaStream_t stream;
+    void* buffers[2];
+    cv::Mat decodedImage;
+    cv::Mat resizedImage;
+    cv::Mat preprocessedImage;
+    std::unique_ptr<float[]> data {nullptr};
+    std::unique_ptr<float[]> prob {nullptr};
+
+    IRuntime* runtime = nullptr;
+    ICudaEngine* engine = nullptr;
+    IExecutionContext* context = nullptr;
+
+    const int deviceID = 0;
+    const double nmsThresh = 0.4;
+    const double confidenceThresh = 0.5;
+    
+    const std::string engineName = "uavship.engine";
+    const int batchSize = 1;
+    const int inputWidth = Yolo::INPUT_W;
+    const int inputHeight = Yolo::INPUT_H;
+    const int maxOutputBBoxCount = Yolo::MAX_OUTPUT_BBOX_COUNT;
+    const int classNumber = Yolo::CLASS_NUM;
+    const int outputSize = Yolo::MAX_OUTPUT_BBOX_COUNT * sizeof(Yolo::Detection) / sizeof(float) + 1;
+
+    const char* inputBlobName = "data";
+    const char* outputBlobName = "prob";
+
+    Logger gLogger;
+};
+
+
+} // namespace detsvr
+
+#endif
